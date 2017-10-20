1e3c343ebec7d392cd43dd1184bda5b437f486d4
jens3737 <jenshernalsteen@gmail.com>
2017-10-21 01:34:20 +0200

    beta versie

--- src/scripts/ThessaliaBuyer.java
+++ src/scripts/ThessaliaBuyer.java
@@ -2,26 +2,145 @@ package scripts;
 import org.powerbot.script.Condition;
 import org.powerbot.script.PollingScript;
 import org.powerbot.script.Script;
+import org.powerbot.script.Area;
 import org.powerbot.script.Tile;
 import org.powerbot.script.rt4.*;
-import utils.DelayUtil;
 
 @Script.Manifest(name = "Hello, RSBot!", properties = "author=Jens; topic=1296203; client=4;", description = "A 'Hello, World' example for RSBot" )
 public class ThessaliaBuyer extends PollingScript<ClientContext> {
 
+    Boolean STOPPED = false;
     final static int THESSALIA_ID = 534;
+    final static Tile TILE_BANK = new Tile(3183, 3435, 0);
+    final static Tile TILE_SHOP = new Tile(3209, 3415, 0);
+    final static Tile[] AREA_SHOP = new Tile[]{
+            new Tile(3208, 3418, 0),
+            new Tile(3208, 3416, 0),
+            new Tile(3208, 3415, 0),
+            new Tile(3207, 3419, 0),
+            new Tile(3207, 3418, 0),
+            new Tile(3207, 3417, 0),
+            new Tile(3207, 3416, 0),
+            new Tile(3207, 3415, 0),
+            new Tile(3207, 3414, 0),
+            new Tile(3207, 3413, 0),
+            new Tile(3207, 3412, 0),
+            new Tile(3206, 3419, 0),
+            new Tile(3206, 3418, 0),
+            new Tile(3206, 3417, 0),
+            new Tile(3206, 3416, 0),
+            new Tile(3206, 3415, 0),
+            new Tile(3206, 3414, 0),
+            new Tile(3205, 3418, 0),
+            new Tile(3205, 3417, 0),
+            new Tile(3205, 3416, 0),
+            new Tile(3205, 3414, 0),
+            new Tile(3204, 3418, 0),
+            new Tile(3204, 3417, 0),
+            new Tile(3204, 3416, 0),
+            new Tile(3204, 3414, 0),
+            new Tile(3203, 3417, 0),
+            new Tile(3203, 3416, 0),
+            new Tile(3203, 3414, 0),
+            new Tile(3202, 3417, 0),
+            new Tile(3202, 3416, 0),
+            new Tile(3202, 3414, 0),
+    };
+
+    final static Tile[] AREA_BANK = new Tile[]{ new Tile(3185, 3433, 0),
+            new Tile(3185, 3434, 0),
+            new Tile(3185, 3435, 0),
+            new Tile(3185, 3436, 0),
+            new Tile(3185, 3437, 0),
+            new Tile(3185, 3438, 0),
+            new Tile(3185, 3439, 0),
+            new Tile(3185, 3440, 0),
+            new Tile(3185, 3441, 0),
+            new Tile(3185, 3442, 0),
+            new Tile(3185, 3443, 0),
+            new Tile(3185, 3444, 0),
+            new Tile(3185, 3445, 0),
+            new Tile(3185, 3446, 0),
+            new Tile(3185, 3447, 0),
+            new Tile(3185, 3448, 0),
+            new Tile(3184, 3433, 0),
+            new Tile(3184, 3434, 0),
+            new Tile(3184, 3435, 0),
+            new Tile(3184, 3436, 0),
+            new Tile(3184, 3437, 0),
+            new Tile(3184, 3438, 0),
+            new Tile(3184, 3439, 0),
+            new Tile(3184, 3440, 0),
+            new Tile(3184, 3441, 0),
+            new Tile(3184, 3442, 0),
+            new Tile(3184, 3443, 0),
+            new Tile(3184, 3444, 0),
+            new Tile(3184, 3445, 0),
+            new Tile(3184, 3446, 0),
+            new Tile(3183, 3433, 0),
+            new Tile(3183, 3434, 0),
+            new Tile(3183, 3435, 0),
+            new Tile(3183, 3436, 0),
+            new Tile(3183, 3437, 0),
+            new Tile(3183, 3438, 0),
+            new Tile(3183, 3439, 0),
+            new Tile(3183, 3440, 0),
+            new Tile(3183, 3441, 0),
+            new Tile(3183, 3442, 0),
+            new Tile(3183, 3443, 0),
+            new Tile(3183, 3444, 0),
+            new Tile(3183, 3445, 0),
+            new Tile(3183, 3446, 0),
+            new Tile(3183, 3447, 0),
+            new Tile(3182, 3433, 0),
+            new Tile(3182, 3434, 0),
+            new Tile(3182, 3435, 0),
+            new Tile(3182, 3436, 0),
+            new Tile(3182, 3437, 0),
+            new Tile(3182, 3438, 0),
+            new Tile(3182, 3439, 0),
+            new Tile(3182, 3440, 0),
+            new Tile(3182, 3441, 0),
+            new Tile(3182, 3442, 0),
+            new Tile(3182, 3443, 0),
+            new Tile(3182, 3444, 0),
+            new Tile(3182, 3445, 0),
+            new Tile(3182, 3446, 0),
+            new Tile(3182, 3447, 0),
+            new Tile(3181, 3433, 0),
+            new Tile(3181, 3434, 0),
+            new Tile(3181, 3435, 0),
+            new Tile(3181, 3436, 0),
+            new Tile(3181, 3437, 0),
+            new Tile(3181, 3438, 0),
+            new Tile(3181, 3439, 0),
+            new Tile(3181, 3440, 0),
+            new Tile(3181, 3441, 0),
+            new Tile(3181, 3442, 0),
+            new Tile(3181, 3443, 0),
+            new Tile(3181, 3444, 0),
+            new Tile(3181, 3445, 0),
+            new Tile(3181, 3446, 0),
+            new Tile(3180, 3434, 0),
+            new Tile(3180, 3437, 0),
+            new Tile(3180, 3439, 0),
+            new Tile(3180, 3441, 0),
+            new Tile(3180, 3445, 0),
+            new Tile(3180, 3446, 0)
+    };
     final static int GYPSY_ID = 5082;
     final static int BANKER_ID = 2897;
     final Component PinkSkirt = ctx.widgets.widget(300).component(2).component(6);
-    Boolean switchWorld = false;
+    final Component RedCape = ctx.widgets.widget(300).component(2).component(9);
+    final Component BrownApron = ctx.widgets.widget(300).component(2).component(5);
     final static int[] Worlds = {0, 6, 12, 18, 24, 36, 42, 48, 60, 66};
     static int indexWorlds = 0;
     final static int CLOSEDDOOR_ID = 11775;
     final static int OPENDOOR_ID = 11774;
-    static Boolean firstSwitch = true;
     final static int PINKSKIRT_ID = 1013;
-//    public static final int WIDGET_STORE_CLOSE_BUTTON = 86;
-//    public static final int WIDGET_STORE = 300;
+    final static int BROWNAPRON_ID = 1757;
+    final static int REDCAPE_ID = 1007;
+
     @Override
     public void start() {
         log.info("script started, RSBot!");
@@ -36,61 +155,74 @@ public class ThessaliaBuyer extends PollingScript<ClientContext> {
     @Override
     public void poll() {
         while(!isInventoryFull()) {
-            if (!isThessaliaInViewport()) {
-                while (!moveToShop()){
-                    moveToShop();
+            if (!isPlayerInArea(AREA_SHOP)) {
+                if(moveToShop()){
+                    if(!isDoorOpen()){
+                        log.info("deur is toe");
+                        delay(100);
+                        openDoor(1);
+                        delay(400);
+                        buyItems();
+                    }
+                    else {
+                        log.info("deur is open");
+                        buyItems();
+                        delay(200);
+                    }
                 }
-                if(!isDoorOpen()){
-                    delay(100);
-                    openDoor();
-                    delay(400);
-                    buySkirt();
-                    switchWorld();
-                }
-                else {
-                    buySkirt();
-                    switchWorld();
-                }
+//
+//                while (!moveToShop()){
+//                    moveToShop();
+//                }
+
             }
             else{
-                if(!isDoorOpen()){
-                    openDoor();
-                    buySkirt();
-                    switchWorld();
-                }
-                else {
-                    buySkirt();
-                    switchWorld();
-                }
+                buyItems();
+//                if(!isDoorOpen()){
+//                    log.info("deur is toe");
+//                    openDoor(1);
+//                    buyItems();
+//                }
+//                else {
+//                    log.info("deur is open");
+//                    buyItems();
+//                    delay(200);
+//                }
             }
         }
 
         while(isInventoryFull()){
-            if(isDoorOpenExit()){
-                while (!moveToBank()){
-                    moveToBank();
+            //kijk of je in shop bent
+            if(isPlayerInArea(AREA_SHOP)){
+                log.info("player is in shop & inventory is full");
+                if(!isDoorOpenExit()){
+                    log.info("deur is toe");
+                    openDoor(0);
                 }
+                else{
+                    log.info("deur is open");
+                    if(moveToBank()){
+                        log.info("je bent in de bank");
+                        depositToBank();
+                    }
+                }
+            }
+            //indien speler in bank is
+            else if(isPlayerInArea(AREA_BANK)){
+                log.info("je bent in de bank");
                 depositToBank();
             }
-            else{
-                openDoor();
-                while (!moveToBank()){
-                    moveToBank();
+            //indien ergens tussenin, beweeg naar bank
+            else {
+                if(moveToBank()){
+                    log.info("je bent in de bank");
+                    depositToBank();
                 }
-                depositToBank();
             }
-
         }
     }
 
-    public void openDoor(){
-//        //check if door isn't already open
-//        if(!isDoorOpen()){
-            GameObject shopClosedDoor = ctx.objects.select().id(11775).nearest().poll();
-            shopClosedDoor.interact("Open");
-            delay(500);
-//        }
-    }
+
 
     /*----------- Bools -----------*/
     public Boolean isShopOpen() {
@@ -120,6 +252,38 @@ public class ThessaliaBuyer extends PollingScript<ClientContext> {
         }
     }
 
+    public Boolean isPlayerInArea(Tile[] area){
+        Boolean result = false;
+        for (int i = 0; i < area.length; i++){
+            if(area[i].toString().equals(ctx.players.local().tile().toString())){
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    public Boolean isPlayerInFrontofShop(){
+        if(TILE_SHOP.toString().equals(ctx.players.local().tile().toString())){
+            return true;
+        }
+
+        else  {
+            log.info(".local().tile()"+ctx.players.local().tile().toString());
+            return false;
+        }
+    }
+
+    public Boolean isPlayerInBank(){
+        if(TILE_BANK.toString().equals(ctx.players.local().tile().toString())){
+            return true;
+        }
+
+        else  {
+            log.info(".local().tile()"+ctx.players.local().tile().toString());
+            return false;
+        }
+    }
+
     public Boolean isThessaliaInViewport() {
         Npc Thessalia = ctx.npcs.select().id(THESSALIA_ID).nearest().poll();
         return Thessalia.inViewport();
@@ -130,45 +294,91 @@ public class ThessaliaBuyer extends PollingScript<ClientContext> {
     public void closeShop(int milisec){
         delay(milisec);
         Component widgetExit = ctx.widgets.widget(300).component(1).component(3);
-        widgetExit.click();
+        while(isShopOpen()){
+            widgetExit.click();
+        }
+
     }
 
-    public void buySkirt() {
+    //je bent binnen  = 0, buiten == 1
+    public void openDoor(int location){
+//        //check if door isn't already open
+//        if(!isDoorOpen()){
+        switch (location) {
+            case 1:
+                while (!isDoorOpen()) {
+                    GameObject shopClosedDoor = ctx.objects.select().id(11775).nearest().poll();
+                    shopClosedDoor.interact("Open");
+                    delay(1200);
+                }
+                break;
+            case 0:
+                while (!isDoorOpenExit()) {
+                    GameObject shopClosedDoor = ctx.objects.select().id(11775).nearest().poll();
+                    shopClosedDoor.interact("Open");
+                    delay(1200);
+                }
+                break;
+
+//        }
+        }
+    }
+
+    public void interactNpc(Npc npc, String option){
+        ctx.camera.turnTo(npc);
+        npc.interact(option);
+        delay(2000);
+    }
+
+    public void doBuy(){
+            while (PinkSkirt.itemStackSize() > 0 && !isInventoryFull()) {
+                delay(1500);
+                PinkSkirt.interact("Buy 5", "Pink skirt");
+            }
+            while (RedCape.itemStackSize() > 0 && !isInventoryFull()) {
+                delay(500);
+                RedCape.interact("Buy 5", "Red cape");
+            }
+            while(BrownApron.itemStackSize()>0 && !isInventoryFull()){
+                delay(500);
+                BrownApron.interact("Buy 5", "Brown apron");
+            }
+            //if no stock --> close widget
+        if(!isInventoryFull()){
+            closeShop(800);
+            switchWorld();
+        }
+        else closeShop(800);
+
+    }
+
+    public void buyItems() {
         log.info("rokje kopen");
             //trade Thessalia if widget is closed
             if (!isShopOpen()) {
                 //Trade Thessalia
-
-                while(!isShopOpen()){
-                    final Npc Thessalia = ctx.npcs.select().id(THESSALIA_ID).nearest().poll();
-                    ctx.camera.turnTo(Thessalia);
-                    Thessalia.interact("Trade");
-                    delay(2000);
+                final Npc Thessalia = ctx.npcs.select().id(THESSALIA_ID).nearest().poll();
+                while(!isShopOpen()) {
+                    interactNpc(Thessalia, "Trade");
                 }
                 //Buy if stock > 0
-                if (PinkSkirt.itemStackSize() > 0) {
-                    delay(1500);
-                    PinkSkirt.interact("Buy 5", "Pink skirt");
-
-                    //hierna mag world switch
-                    closeShop(800);
-                }
-                //if no stock --> close widget
-                else{
-                    closeShop(800);
-                }
+                doBuy();
+                delay(5000);
             }
 
             //Widget is open, close it
-            else {
-                closeShop(800);
+            else if(isShopOpen()) {
+                doBuy();
+                delay(5000);
             }
     }
 
     public void switchWorld(){
         log.info("wereldje switchen");
         //beweeg muis naar logout en klik
+        //laptop
         Component logoutBtn = ctx.widgets.widget(161).component(38);
+//        Component logoutBtn = ctx.widgets.widget(548).component(33);
         delay(600);
         logoutBtn.click();
 
@@ -200,22 +410,28 @@ public class ThessaliaBuyer extends PollingScript<ClientContext> {
         }
     }
 
+    public void doDeposit(){
+        ctx.bank.deposit(PINKSKIRT_ID, 27);
+        delay(200);
+        ctx.bank.deposit(REDCAPE_ID, 27);
+        delay(200);
+        ctx.bank.deposit(BROWNAPRON_ID, 27);
+        delay(200);
+        ctx.bank.close();
+    }
+
     public Boolean depositToBank(){
         log.info("Deposit inventory to bank");
         final Npc Banker = ctx.npcs.select().id(BANKER_ID).nearest().poll();
         // zolang bank tab niet open is, interact met banker
         while(!ctx.bank.open()){
-            delay(200);
-            Banker.interact("Bank");
-            delay(300);
+            interactNpc(Banker, "Bank");
         }
 
         //Wanneer bank tab open is, deposit inventory
         while(ctx.bank.open()){
-            ctx.bank.deposit(1013, 27);
-            delay(200);
-            ctx.bank.close();
-            log.info("true");
+            doDeposit();
+            delay(800);
             return true;
         }
         return false;
@@ -230,24 +446,17 @@ public class ThessaliaBuyer extends PollingScript<ClientContext> {
                 new Tile(3188, 3430, 0),
                 new Tile(3183,3435, 0)};
         TilePath pathToBank = ctx.movement.newTilePath(routeBank);
-        final Npc Banker = ctx.npcs.select().id(BANKER_ID).nearest().poll();
-
         delay(100);
 
-        //Indien bank nie tin viewPort, loop richting bank
-        if(!ctx.bank.inViewport()){
-
+        while(!isPlayerInBank()){
             pathToBank.traverse();
-            return false;
+            delay(500);
         }
-        //indien bank wel in viewport, draai camera naar Banker
-        else {
-            pathToBank.traverse();
-            final Npc Banker = ctx.npcs.select().id(BANKER_ID).nearest().poll();
-            ctx.camera.turnTo(Banker);
-            delay(2500);
+        if(isPlayerInBank()) {
+            log.info("ik ben in de bank");
             return true;
         }
+        else return false;
     }
 
     public Boolean moveToShop(){
@@ -261,16 +470,26 @@ public class ThessaliaBuyer extends PollingScript<ClientContext> {
         TilePath pathToShop = ctx.movement.newTilePath(routeShop);
         delay(100);
 
-
-        if(!isThessaliaInViewport()){
+        while(!isPlayerInFrontofShop()){
             pathToShop.traverse();
-            return false;
+            delay(500);
         }
-        else {
-            pathToShop.traverse();
-            delay(2500);
+        if(isPlayerInFrontofShop()){
             return true;
         }
+        else return false;
+
+
+//        //
+//        if(!isThessaliaInViewport()){
+//            pathToShop.traverse();
+//            return false;
+//        }
+//        else {
+//            pathToShop.traverse();
+//            delay(2500);
+//            return true;
+//        }
     }
 
     public void delay(int milisec) {
